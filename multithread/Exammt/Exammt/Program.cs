c3a976219bfa2c8328c63819f5ae804582651d86
Dima <wot_ukr@ukr.net>
2021-01-12 13:19:11 +0200

    rly true multithread

---
+++ multithread/Exammt/Exammt/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Exammt
+{
+    class Program
+    {
+        static void Main (string[] args)
+        
+        {
+            while (true)
+            {
+
+                var input = Console.ReadLine();
+                var cancelTokenSource = new CancellationTokenSource();
+                Task.Run(() => Delay(input, cancelTokenSource.Token));
+                do
+                {
+                }
+                while (Console.ReadLine() != "");
+
+
+
+                cancelTokenSource.Cancel();
+            }
+        }
+
+        private static async Task Delay(string input, CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                Console.WriteLine(input);
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
